e1d7a14c88d8ec94daaa4b7e145054b830e181d7
John Hassell <john.hassell@waterfield.com>
2019-06-28 22:50:27 -0500

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :assignments
   resources :committees
   resources :members
-  root to: 'visitors#index'
+  root to: 'assignments#index'
   devise_for :users
   resources :users
 end
